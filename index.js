7b110a6950b76e377f7adaab9bb0fe2a553bd0d0
AhmedAbd-Allah <ahmedabdallah.programming@gmail.com>
2021-02-06 12:50:10 +0200

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const mongoSanitize = require('express-mongo-sanitize');
 const bodyParser = require('body-parser');
 
 
 
+// Start database connection
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true
+            })
+    } catch (error) {
+        console.error(error)
+    }
+})()
+
+
+// parse request body
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: false }));
+
+
+// sanitize requests  to prevent MongoDB Operator Injection
+app.use(mongoSanitize());
 
 
 // allow cross origin
@@ -17,6 +40,13 @@ app.use(function (req, res, next) {
     );
     next();
 });
+
+// app.get('/contacts', contactController.getAllContacts)
+// app.get('/contact/:contactId', contactController.getContact)
+// app.post('/contact', contactController.createContact)
+// app.delete('/contact/:contactId', contactController.deleteContact)
+// app.patch('/contact/:contactId', contactController.updateContact)
+
 
 // server up
 app.listen(process.env.SERVER_PORT, () => {
